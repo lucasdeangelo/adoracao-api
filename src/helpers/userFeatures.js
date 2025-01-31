a6b864d02859542da47a4135e5a7d15ba02927fc
Lucas de Angelo <lucasdeangelo343@gmail.com>
2025-01-31 13:54:29 -0300

    Add - Adiciona .gitignore para ignorar arquivos sensíveis

--- src/helpers/userFeatures.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-function generateToken(id_login, email_user){
-  const secret = '1501222724' 
-  return jwt.sign({infoUser: {id_login, email_user}}, secret, {expiresIn: 60 * 60});
-}
-
-export {generateToken};
